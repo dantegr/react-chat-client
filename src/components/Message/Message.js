0e7f70ac6a534bdb79f0bdd0263ab69250e5afbb
Pantelis Tamtakos <you@example.com>
2021-05-31 17:30:54 +0200

    css changes

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -13,7 +13,7 @@ const Message = ({ message: { user, text }, name }) => {
   return isSentByCurrentUser ? (
     <div className="messageContainer justifyEnd">
       <p className="sentText pr-10">{trimmedName}</p>
-      <div className="messageBox backgroundBlue">
+      <div className="messageBox backgroundGreen">
         <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
       </div>
     </div>
